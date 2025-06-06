b3354f760b7496814f63c6aeb910d168f12488f6
isadoraalbano <isadoraalbano6@gmail.com>
2025-06-06 16:07:56 -0300

    Fix: ajustes finais atividade vivencial

--- src/AtividadesVivenciais/AtividadeVivencial3105.cpp
+++ src/AtividadesVivenciais/AtividadeVivencial3105.cpp
@@ -1,110 +1,126 @@
 #include <iostream>
 #include <string>
+#include <assert.h>
+#include <cmath>
 #include <vector>
+
+using namespace std;
+
+// GLAD
 #include <glad/glad.h>
+
+// GLFW
 #include <GLFW/glfw3.h>
 
+// STB_IMAGE
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-
-
+// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
-using namespace std;
 using namespace glm;
 
+// Protótipo da função de callback de teclado
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
+// Protótipos das funções
+int setupShader();
+int createVAO();
+int loadTexture(string filePath);
 
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
-
+// Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+const GLchar *vertexShaderSource = R"(
+ #version 400
+ layout (location = 0) in vec3 position;
+ layout (location = 1) in vec2 texc;
+ out vec2 tex_coord;
+ uniform mat4 projection;
+ uniform mat4 model;
+ uniform float offsetX;
+ uniform float textureWidth;
 
-const char* vertexShaderSource = R"(
-#version 400
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 texCoord;
+ void main()
+ {
+    tex_coord.x = texc.x + (offsetX / textureWidth);
+    tex_coord.y = texc.y;
 
+    gl_Position = projection * model * vec4(position, 1.0);
+ }
+ )";
 
-uniform mat4 projection;
-uniform float offsetX;
-
-
-out vec2 TexCoord;
-
-
-void main()
-{
-    vec3 pos = position;
-    pos.x += offsetX;
-    gl_Position = projection * vec4(pos, 1.0);
-    TexCoord = texCoord;
-}
-)";
-
-
-const char* fragmentShaderSource = R"(
-#version 400
-in vec2 TexCoord;
-out vec4 FragColor;
-uniform sampler2D texture1;
-
-
-void main()
-{
-    FragColor = texture(texture1, TexCoord);
-}
-)";
-
+// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
+const GLchar *fragmentShaderSource = R"(
+ #version 400
+ in vec2 tex_coord;
+ out vec4 color;
+ uniform sampler2D tex_buff;
+ void main()
+ {
+	 color = texture(tex_buff,tex_coord);
+ }
+ )";
 
 struct Layer {
     GLuint textureID;
     float speedFactor;
     float offsetX = 0.0;
+    int width;
+    int height;
 };
 
-
 vector<Layer> layers;
-GLuint quadVAO, quadVBO;
-GLuint shaderProgram;
-
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-int loadTexture(string path);
-void initQuad();
-GLuint createShaderProgram();
-
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	// Inicialização da GLFW
+	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Parallax Scrolling", nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
+	glfwWindowHint(GLFW_SAMPLES, 8);
 
+	// Criação da janela GLFW
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "M4", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Falha ao criar a janela GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	// Fazendo o registro da função de callback para a janela GLFW
+	glfwSetKeyCallback(window, key_callback);
 
+	// GLAD: carrega todos os ponteiros d funções da OpenGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Falha ao inicializar GLAD" << std::endl;
+		return -1;
+	}
 
-    glViewport(0, 0, WIDTH, HEIGHT);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
 
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 
-    shaderProgram = createShaderProgram();
-    initQuad();
+	GLuint shaderID = setupShader();
+    GLuint VAO = createVAO();
 
-
-    const string textures[] = {
+	const string textures[] = {
         "../assets/sprites/sky.png",
         "../assets/sprites/clouds_1.png",
         "../assets/sprites/clouds_2.png",
@@ -114,155 +130,213 @@ int main()
         "../assets/sprites/rocks_2.png"
     };
 
-
-    float speeds[] = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.8, 1.0 };
-
+	float speeds[] = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.8, 1.0 };
 
     for (int i = 0; i < 7; i++) {
         Layer layer;
         layer.textureID = loadTexture(textures[i]);
         layer.speedFactor = speeds[i];
+        layer.width = 800;
+        layer.height = 600  ;
         layers.push_back(layer);
     }
 
+	
+	GLuint vampireTexture = loadTexture("../assets/sprites/Vampirinho.png");
 
-    GLuint vampTex = loadTexture("../assets/sprites/Vampirinho.png");
+	glUseProgram(shaderID);
 
+	float colorValue = 0.0;
 
-    mat4 projection = glm::ortho(0.0, 800.0, 600.0 , 0.0, -1.0, 1.0);
+	// Ativando o primeiro buffer de textura do OpenGL
+	glActiveTexture(GL_TEXTURE0);
 
+	// Criando a variável uniform pra mandar a textura pro shader
+	glUniform1i(glGetUniformLocation(shaderID, "tex_buff"), 0);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-        glClearColor(0, 0, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST); 
+	glDepthFunc(GL_ALWAYS);	 
 
+	glEnable(GL_BLEND);			  
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(quadVAO);
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, value_ptr(projection));
+	mat4 projection = ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
+	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
+    GLint textureWidthLoc = glGetUniformLocation(shaderID, "textureWidth");
 
+	// Loop da aplicação - "game loop"
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
 
-        for (auto& layer : layers) {
-            glBindTexture(GL_TEXTURE_2D, layer.textureID);
-            glUniform1f(glGetUniformLocation(shaderProgram, "offsetX"), layer.offsetX);
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-        }
+		// Limpa o buffer de cor
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // cor de fundo
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glBindVertexArray(VAO);
+		glLineWidth(10);
+		glPointSize(20);
 
-        // Vampirinho no centro
-        glBindTexture(GL_TEXTURE_2D, vampTex);
-        glUniform1f(glGetUniformLocation(shaderProgram, "offsetX"), 0.0);
+         mat4 model = mat4(1.0);
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
+
+		for (Layer &layer : layers)
+		{
+            glBindTexture(GL_TEXTURE_2D, layer.textureID); // Conectando ao buffer de textura
+            glUniform1f(glGetUniformLocation(shaderID, "offsetX"), layer.offsetX);
+            glUniform1f(textureWidthLoc, (float)layer.width);
+			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		}
+
+        mat4 model2 = mat4(1.0);
+        model2 = glm::translate(model2, vec3(400.0, 50.0, 0.0));
+        model2 = glm::scale(model2, glm::vec3(100.0 / 800.0, 100.0 / 600.0, 1.0));
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model2));
+        
+        glBindTexture(GL_TEXTURE_2D, vampireTexture);
+        glUniform1f(glGetUniformLocation(shaderID, "offsetX"), 0.0);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+		glfwSwapBuffers(window);
+	}
 
-        glfwSwapBuffers(window);
-    }
-
-
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
-
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (action == GLFW_PRESS || action == GLFW_REPEAT) {
-        double delta = 10.0;
+        double movementAmount = 10.0;
         if (key == GLFW_KEY_LEFT || key == GLFW_KEY_A) {
             for (auto& layer : layers) {
-                layer.offsetX += delta * layer.speedFactor;
+                layer.offsetX -= movementAmount * layer.speedFactor;
             }
         }
         if (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D) {
             for (auto& layer : layers) {
-                layer.offsetX -= delta * layer.speedFactor;
+                layer.offsetX += movementAmount * layer.speedFactor;
             }
         }
     }
 }
 
-
-GLuint createShaderProgram()
+//  A função retorna o identificador do programa de shader
+int setupShader()
 {
-    GLint success;
-    char infoLog[512];
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
 
-    GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertex);
-    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-        cout << "Vertex Shader Error: " << infoLog << endl;
-    }
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Checando erros de compilação (exibição via log no terminal)
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	// Linkando os shaders e criando o identificador do programa de shader
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Checando por erros de linkagem
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
-
-    GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragment);
-    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-        cout << "Fragment Shader Error: " << infoLog << endl;
-    }
-
-
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertex);
-    glAttachShader(program, fragment);
-    glLinkProgram(program);
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
-    return program;
+	return shaderProgram;
 }
 
-
-void initQuad()
+int createVAO()
 {
-    float quad[] = {
+	GLfloat vertices[] = {
         0, 0, 0.0, 0.0, 0.0,
         0, 600, 0.0, 0.0, 1.0,
         800, 0, 0.0, 1.0, 0.0,
         800, 600, 0.0, 1.0, 1.0
     };
 
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	return VAO;
 }
 
-
-int loadTexture(string path)
+int loadTexture(string filePath)
 {
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
-    if (data) {
-        GLenum format = nrChannels == 4 ? GL_RGBA : GL_RGB;
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        stbi_image_free(data);
-    } else {
-        cout << "Failed to load texture: " << path << endl;
-    }
-    return textureID;
+	GLuint texID;
+
+	glGenTextures(1, &texID);
+	glBindTexture(GL_TEXTURE_2D, texID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+
+	unsigned char *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		if (nrChannels == 3) // jpg, bmp
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		}
+		else // png
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		}
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return texID;
 }
-
-
