a53c2067cefc42690e31e5a50d0a83467287f1f3
isadoraalbano <isadoraalbano6@gmail.com>
2025-06-15 20:49:55 -0300

    Fix: ajustes função main

--- src/Texturizacoes/Texturizacoes.cpp
+++ src/Texturizacoes/Texturizacoes.cpp
@@ -21,33 +21,33 @@ int loadTexture(string filePath);
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
-const GLchar* vertexShaderSource = R"(
-    #version 400
-    layout (location = 0) in vec3 position;
-    layout (location = 1) in vec2 texCoord;
+// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+const GLchar *vertexShaderSource = R"(
+ #version 400
+ layout (location = 0) in vec3 position;
+ layout (location = 1) in vec2 texc;
+ out vec2 tex_coord;
+ uniform mat4 projection;
+ uniform mat4 model;
+ void main()
+ {
+	tex_coord = texc;
+	gl_Position = projection * model * vec4(position.x, position.y, position.z, 1.0);
+ }
+ )";
 
-    out vec2 TexCoord;
-
-    uniform mat4 model;
-    uniform mat4 projection;
-
-    void main() {
-        TexCoord = texCoord;
-        gl_Position = projection * model * vec4(position, 1.0);
-    }
-)";
-
-const GLchar* fragmentShaderSource = R"(
-    #version 400
-    in vec2 TexCoord;
-    out vec4 color;
-    uniform sampler2D tex_buff;
-
-    void main() {
-        color = texture(tex_buff, TexCoord);
-    }
-)";
-
+// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
+const GLchar *fragmentShaderSource = R"(
+ #version 400
+ in vec2 tex_coord;
+ out vec4 color;
+ uniform sampler2D tex_buff;
+ void main()
+ {
+	 color = texture(tex_buff,tex_coord);
+ }
+ )";
+ 
 class Sprite {
 private:
     GLuint VAO;
@@ -85,6 +85,67 @@ public:
     void SetSize(vec2 s) { size = s; }
     void SetRotation(float rot) { rotation = rot; }
 };
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Texturizacoes", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    GLuint shaderProgram = setupShader();
+    
+    vector<Sprite> sprites = {
+        Sprite("../assets/sprites/sky_back.png"),
+        Sprite("../assets/sprites/aurora.png"),
+        Sprite("../assets/sprites/clouds_1.png"),
+        Sprite("../assets/sprites/clouds_2.png"),
+        Sprite("../assets/sprites/rocks_tex.png"),
+        Sprite("../assets/sprites/coruja.png"),
+        Sprite("../assets/sprites/vampirinho.png")
+    };
+
+    sprites[0].SetPosition(vec2(400, 300)); sprites[0].SetSize(vec2(800, 600));
+    sprites[1].SetPosition(vec2(400, 300)); sprites[1].SetSize(vec2(800, 600));
+    sprites[2].SetPosition(vec2(150, 100)); sprites[2].SetSize(vec2(200, 150));
+    sprites[3].SetPosition(vec2(650, 150)); sprites[3].SetSize(vec2(200, 150));
+    sprites[4].SetPosition(vec2(400, 500)); sprites[4].SetSize(vec2(800, 200));
+    sprites[5].SetPosition(vec2(200, 400)); sprites[5].SetSize(vec2(100, 100));
+    sprites[6].SetPosition(vec2(600, 400)); sprites[6].SetSize(vec2(100, 100));
+
+    glUseProgram(shaderProgram);
+    
+    mat4 projection = ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
+
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, value_ptr(projection));
+    glUniform1i(glGetUniformLocation(shaderProgram, "tex_buff"), 0);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        for (auto& sprite : sprites) {
+            sprite.Draw(shaderProgram);
+        }
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
 
 int setupShader() {
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -143,9 +204,6 @@ int loadTexture(string filePath) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-    stbi_set_flip_vertically_on_load(true); 
     
     int width, height, nrChannels;
     unsigned char* data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
@@ -168,63 +226,3 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Texturizacoes", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        cout << "Failed to initialize GLAD" << endl;
-        return -1;
-    }
-
-    GLuint shaderProgram = setupShader();
-    
-    vector<Sprite> sprites = {
-        Sprite("../assets/sprites/sky_back.png"),
-        Sprite("../assets/sprites/aurora.png"),
-        Sprite("../assets/sprites/clouds_1.png"),
-        Sprite("../assets/sprites/clouds_2.png"),
-        Sprite("../assets/sprites/rocks_tex.png"),
-        Sprite("../assets/sprites/coruja.png"),
-        Sprite("../assets/sprites/vampirinho.png")
-    };
-
-    sprites[0].SetPosition(vec2(400, 300)); sprites[0].SetSize(vec2(800, 600));
-    sprites[1].SetPosition(vec2(400, 300)); sprites[1].SetSize(vec2(800, 600));
-    sprites[2].SetPosition(vec2(150, 500)); sprites[2].SetSize(vec2(200, 150));
-    sprites[3].SetPosition(vec2(650, 450)); sprites[3].SetSize(vec2(200, 150));
-    sprites[4].SetPosition(vec2(400, 100)); sprites[4].SetSize(vec2(800, 200));
-    sprites[5].SetPosition(vec2(200, 200)); sprites[5].SetSize(vec2(100, 100));
-    sprites[6].SetPosition(vec2(600, 200)); sprites[6].SetSize(vec2(100, 100));
-
-    glUseProgram(shaderProgram);
-    
-    mat4 projection = ortho(0.0, 800.0, 0.0, 600.0, -1.0, 1.0);
-
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, value_ptr(projection));
-    glUniform1i(glGetUniformLocation(shaderProgram, "tex_buff"), 0);
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        for (auto& sprite : sprites) {
-            sprite.Draw(shaderProgram);
-        }
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
-    return 0;
-}
